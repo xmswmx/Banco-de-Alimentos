6530ebcdc22223dbda04c367b9b626124bc00e95
Emanuel <emanuelbastons@gmail.com>
2019-10-20 21:16:05 -0300

    Agrego Vista de Registrar-Beneficiario

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { TodolistComponent } from './modules/beneficiario/todolist/todolist.comp
 import { NewItemComponent } from './modules/beneficiario/new-item/new-item.component';
 import { HomeComponent } from './modules/home/home.component';
 import { RegistrarDonanteComponent } from './modules/donante/registrar-donante/registrar-donante.component';
+import { RegistrarBeneficiarioComponent } from './modules/beneficiario/registrar-beneficiario/registrar-beneficiario.component';
 
 const routes: Routes = [
 	{path:'',redirectTo:'/home',pathMatch:'full'},
 	{path:'home',component:HomeComponent},
 	{path:'registrar-donante',component:RegistrarDonanteComponent},
+	{path:'registrar-beneficiario',component:RegistrarBeneficiarioComponent},
 	{path:'new-item',component:NewItemComponent}
 ];
 
