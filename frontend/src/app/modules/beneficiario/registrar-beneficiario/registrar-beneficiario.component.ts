0eb118aac8e9b84105dce07f31ce6ee3581dd34c
Emanuel <emanuelbastons@gmail.com>
2019-10-27 17:50:54 -0300

    Completo el controlador del Form Beneficiario

--- frontend/src/app/modules/beneficiario/registrar-beneficiario/registrar-beneficiario.component.ts
+++ frontend/src/app/modules/beneficiario/registrar-beneficiario/registrar-beneficiario.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Beneficiario } from '../../../_services/lbservice/models';
+import { LoopBackConfig, BaseLoopBackApi } from '../../../_services/lbservice';
+
+//Voy a usar el model que traje desde LB, tal vez haya que hacer una clase nueva en Angular
+import { Beneficiario } from '../../../_services/lbservice/models';  
+
+import { Route } from '@angular/compiler/src/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { BeneficiarioApi } from '../../../_services/lbservice/services';
 
 @Component({
   selector: 'app-registrar-beneficiario',
@@ -9,15 +17,32 @@ import { Beneficiario } from '../../../_services/lbservice/models';
 })
 export class RegistrarBeneficiarioComponent implements OnInit {
 
-/*
-	this.registrarDonanteForm = new FormGroup({
-       nombreOrganizacion: new FormControl()
-    });"
-*/
-  constructor() { }
+	registrarBeneficiarioForm: FormGroup;
+
+	
+	
+
+
+	//Creo un beneficiario nuevo
+	nuevoBeneficiario : Beneficiario;
+
+  constructor(private beneficiarioApi: BeneficiarioApi, private route: ActivatedRoute , private router:Router) {
+	
+	this.nuevoBeneficiario =  new Beneficiario();
+	
+	this.registrarBeneficiarioForm = new FormGroup({
+       nombreOrganizacion: new FormControl(),
+	   adress: new FormControl(),
+	   email: new FormControl(),
+	   password1: new FormControl(),
+	   password2: new FormControl()
+    });
+
+  }
 
   onSubmit(){
 	//var beneficiario = new Beneficiario();
+	//this.nuevoBeneficiario = new Beneficiario();
 	console.log("Aca se tendria que crear un beneficiario nuevo");
   }
   
