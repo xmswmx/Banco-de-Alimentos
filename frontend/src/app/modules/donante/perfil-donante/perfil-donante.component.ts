9dc84505379fdd2e7fd4c93714895e7a6259205c
Marascutti, Juan Marcos <jmarascutti@hotmail.com>
2019-10-27 20:08:11 -0300

    Incorporacion de vista perfil-donante

---
+++ frontend/src/app/modules/donante/perfil-donante/perfil-donante.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-donante',
+  templateUrl: './perfil-donante.component.html',
+  styleUrls: ['./perfil-donante.component.css']
+})
+export class PerfilDonanteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
