3d721c148717390ebf23bba1823f84ae42c08a17
Emanuel <emanuelbastons@gmail.com>
2019-10-28 08:43:48 -0300

    Login y logout

--- frontend/src/app/Nueva carpeta/_model/donante.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Donante } from './donante';
-
-describe('Donante', () => {
-  it('should create an instance', () => {
-    expect(new Donante()).toBeTruthy();
-  });
-});
--- frontend/src/app/modules/login/login.component.ts
+++ frontend/src/app/modules/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Route } from '@angular/compiler/src/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { User } from '../../_services/lbservice/models';  
+import { UserApi } from '../../_services/lbservice/services';
+import { AccessToken }  from '../../_services/lbservice/models'; 
+import { LoopBackConfig, BaseLoopBackApi } from '../../_services/lbservice';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +13,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  logInForm: FormGroup;
+  user: User;
+  constructor(private userApi: UserApi, private route: ActivatedRoute , private router:Router) { 
+	this.logInForm = new FormGroup({
+	   usuario: new FormControl(),
+	   clave: new FormControl()
+    });
+  }
 
-  constructor() { }
-
+  
+  onSubmit(){
+	  this.user = new User();
+	  this.user.username = this.logInForm.get("usuario").value;
+	  this.user.password = this.logInForm.get("clave").value;
+	  this.userApi.login(this.user).subscribe((token: AccessToken)=> this.router.navigate(['/home']));
+	  
+  }
+  
   ngOnInit() {
   }
 
