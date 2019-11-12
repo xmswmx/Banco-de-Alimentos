3fae7815880bbe629329a9462a5b9752a6ea6ead
Emanuel <emanuelbastons@gmail.com>
2019-11-12 17:27:53 -0300

    Mejoras en registrar-donante

--- frontend/src/app/modules/donante/registrar-donante/registrar-donante.component.ts
+++ frontend/src/app/modules/donante/registrar-donante/registrar-donante.component.ts
@@ -29,10 +29,57 @@ export class RegistrarDonanteComponent implements OnInit {
 		   direccion: new FormControl(),
 		   email: new FormControl(),
 		   password1: new FormControl(),
-		   password2: new FormControl()
+		   password2: new FormControl(),
+		   personaNombre: new FormControl(),
+		   personaApellido: new FormControl(),
+		   personaDni: new FormControl(),
+		   personaEmail: new FormControl(),
+		   personaCelular: new FormControl()
 	    });
 
 
+	}
+
+	agregarContacto(){
+		//Validar los campos
+		if ((this.form.get("personaEmail").value == null) ||
+			(this.form.get("personaApellido").value == null) ||
+			(this.form.get("personaDni").value == null) ||
+			(this.form.get("personaEmail").value == null) ||
+			(this.form.get("personaCelular").value == null)) {
+			alert('Por favor, complete todos los datos solicitados de la persona de contacto para poder registrarla');
+			return 0;
+		}
+
+		//Agregar la persona al Array
+		let nuevaPersona = new PersonaDeContacto;
+		nuevaPersona.nombre = this.form.get("personaNombre").value;
+		nuevaPersona.apellido = this.form.get("personaApellido").value;
+		nuevaPersona.dni = this.form.get("personaDni").value;
+		nuevaPersona.email = this.form.get("personaEmail").value;
+		nuevaPersona.telefono = this.form.get("personaCelular").value;
+		this.colPersonasDeContacto.push(nuevaPersona);
+
+		//Limpiar los campos
+		this.form.get("personaNombre").reset();
+		this.form.get("personaApellido").reset();
+		this.form.get("personaDni").reset();
+		this.form.get("personaEmail").reset();
+		this.form.get("personaCelular").reset();
+
+
+		console.log(this.colPersonasDeContacto);
+	}
+
+	onSubmit(){
+		alert("Vamo a calmarno, todavía no lo terminé 🤗");
+		//Validar campos
+
+		//Postear un donante nuevo con los datos recibidos
+
+		//Para cada contacto de colPersonasDeContacto
+			//Postear contacto
+			//Agregarlo al donante
 	}
 
 	ngOnInit() {
