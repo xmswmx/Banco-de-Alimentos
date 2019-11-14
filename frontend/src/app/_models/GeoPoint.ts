39008cda72c6dc27f3f94300cf19d2e4ba66add4
Emanuel <emanuelbastons@gmail.com>
2019-11-13 21:34:27 -0300

    Fix registrar-persona
    Solucioné un problema que hacía que se abandone la tarea antes de registrar la ubicación

---
+++ frontend/src/app/_models/GeoPoint.ts
@@ -0,0 +1,8 @@
+export class GeoPoint {
+	lat: Number;
+	lng: Number;
+	constructor(lat:Number,lng:Number){
+		this.lat = lat;
+		this.lng = lng;
+	}
+}
--- frontend/src/app/modules/donante/registrar-donante/registrar-donante.component.ts
+++ frontend/src/app/modules/donante/registrar-donante/registrar-donante.component.ts
@@ -6,6 +6,7 @@ import { Route } from '@angular/compiler/src/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
 import { UbicacionApi, DonanteApi, PersonaDeContactoApi } from '../../../_services/lbservice/services';
+import { GeoPoint } from '../../../_services/lbservice/models/BaseModels';
 
 @Component({
   selector: 'app-registrar-donante',
@@ -21,6 +22,7 @@ export class RegistrarDonanteComponent implements OnInit {
 
 	constructor(private ubicacionApi: UbicacionApi, private personasDeContactoApi: PersonaDeContactoApi, private donanteApi: DonanteApi, private route: ActivatedRoute , private router:Router) {
 
+		//Se crea el nuevo donante y los controladores de los campos
 		this.nuevoDonante =  new Donante();
 	
 		this.form = new FormGroup({
@@ -40,6 +42,7 @@ export class RegistrarDonanteComponent implements OnInit {
 
 	}
 
+	//Se invoca para agregar una Persona de Contacto
 	agregarContacto(){
 		//Validar los campos
 		if ((this.form.get("personaEmail").value == null) ||
@@ -50,7 +53,6 @@ export class RegistrarDonanteComponent implements OnInit {
 			alert('Por favor, complete todos los datos solicitados de la persona de contacto para poder registrarla');
 			return 0;
 		}
-
 
 		//Agregar la persona al Array
 		let nuevaPersona = new PersonaDeContacto;
@@ -70,22 +72,7 @@ export class RegistrarDonanteComponent implements OnInit {
 
 	}
 
-/*
-DonanteInterface {
-  "cuil": string;
-  "realm"?: string;
-  "username"?: string;
-  "email": string;
-  "emailVerified"?: boolean;
-  "id"?: any;
-  "password"?: string;
-  accessTokens?: any[];
-  personasDeContacto?: PersonaDeContacto[];
-  ubicacion?: Ubicacion;
-  insignia?: Insignia[];
-  donaciones?: Donacion[];
-}
-*/
+	//Se invoca cuando se "Registra el nuevo donante"
 	onSubmit(){
 		//Validar campos
 		if (this.form.get("password1").value != this.form.get("password2").value) {
@@ -97,43 +84,48 @@ DonanteInterface {
 		this.nuevoDonante = new Donante;
 		this.nuevoDonante.username = this.form.get("razonSocial").value;
 		this.nuevoDonante.cuil = this.form.get("cuil").value;
-		
-
-		//Necesito crearle su ubicacion 
-		let nuevaUbicacion = new Ubicacion;
-		nuevaUbicacion.direccion = this.form.get("direccion").value;
-		this.nuevoDonante.ubicacion = nuevaUbicacion;
-		//nuevaUbicacion.puntoGeografico = this.obtenerGeoPointDe(this.form.get("direccion").value);
-
 		this.nuevoDonante.email = this.form.get("email").value;
 		this.nuevoDonante.password = this.form.get("password1").value;
+
+		/* ¡ARREGLAR!
+		 * Necesito crearle su ubicacion 
+		 * Falta crearle el geopoint, para esto necesito tener una clase Geopoint
+		 * Por ahora hice que la API no me obligue a agregarlo y lo creo solo con su dirección 
+		 */
+		let nuevaUbicacion = new Ubicacion;
+		nuevaUbicacion.direccion = this.form.get("direccion").value;
+		//nuevaUbicacion.puntoGeografico = this.obtenerGeoPointDe(this.form.get("direccion").value);
+		this.nuevoDonante.ubicacion = nuevaUbicacion;
+		
+
+
 
 		//Inicio el array de personas de contacto
 		this.nuevoDonante.personasDeContacto=[];
 
-		/* le pusheo cada persona de la lista
-		 * No funcionó
-				
-		for (let persona of this.colPersonasDeContacto){
-			this.nuevoDonante.personasDeContacto.push(persona);	
-		}
-
-		*/
-
 		//Llamo a la api para crear el donante y con su id creo las otras entidades que le pertenecen
 		this.donanteApi.create(this.nuevoDonante).subscribe((donanteCreado:Donante)=>{
-			//Creo todos sus contactos
-			for (let persona of this.colPersonasDeContacto){
-				persona.idDonante = donanteCreado.id;
-				this.personasDeContactoApi.create(persona);
-			}
 			//Creo su ubicación
 			this.nuevoDonante.ubicacion.idDonante= donanteCreado.id;
-			this.ubicacionApi.create(this.nuevoDonante.ubicacion)
-
-			//Me voy (Una mejora sería asegurarme que se registraron las cosas antes con .subscribe)
-			this.router.navigateByUrl("/login");
-			alert('Se registró exitosamente');
+			this.ubicacionApi.create(this.nuevoDonante.ubicacion).subscribe(()=>{
+				//Creo sus contactos
+				let contadorContactos = 0;
+				let totalDeContactos = this.nuevoDonante.personasDeContacto.length;
+				for (let persona of this.colPersonasDeContacto){
+					persona.idDonante = donanteCreado.id;
+					this.personasDeContactoApi.create(persona).subscribe(()=>{
+						//Si soy el último contacto me voy
+						if (contadorContactos < totalDeContactos) {
+							//No soy el último
+							contadorContactos++
+						} else {
+							//Soy el último
+							this.router.navigateByUrl("/login");
+							alert('Se registró exitosamente');
+						}
+					})
+				}
+			})
 		});			
 	}
 
@@ -141,7 +133,7 @@ DonanteInterface {
 		Colocar acá la fauncion de google para calcular el geopoint
 	*/
 	obtenerGeoPointDe(unaDireccion:String){
-		return 0
+		return 'GeoPoint';
 	}
 
 	ngOnInit() {
