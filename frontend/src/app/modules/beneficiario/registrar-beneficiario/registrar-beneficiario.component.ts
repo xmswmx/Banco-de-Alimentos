c4bbe3b81ee1b339dfef79f42c212569ab4f1166
Matias Mosquera <matias.mosquera@hotmail.com.ar>
2019-11-20 12:11:34 -0300

    Se agregaron validaciones al alta de beneficiario. Se movieron carpetas de poco uso a una llamada 'extras'

--- frontend/src/app/modules/beneficiario/registrar-beneficiario/registrar-beneficiario.component.ts
+++ frontend/src/app/modules/beneficiario/registrar-beneficiario/registrar-beneficiario.component.ts
@@ -1,71 +1,86 @@
 import { Component, OnInit } from '@angular/core';
-
-import { LoopBackConfig, BaseLoopBackApi } from '../../../_services/lbservice';
-
 //Voy a usar el model que traje desde LB, tal vez haya que hacer una clase nueva en Angular
-import { Beneficiario } from '../../../_services/lbservice/models';  
-
-import { Route } from '@angular/compiler/src/core';
+import { Beneficiario, Ubicacion } from '../../../_services/lbservice/models';  
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
-import { BeneficiarioApi } from '../../../_services/lbservice/services';
+import { Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { BeneficiarioApi, UbicacionApi } from '../../../_services/lbservice/services';
+import { AddressConverter } from '../../../_models/AddressConverter';
+import { LoopBackConfig, BaseLoopBackApi } from '../../../_services/lbservice';
+import { Route } from '@angular/compiler/src/core';
 
 @Component({
-  selector: 'app-registrar-beneficiario',
-  templateUrl: './registrar-beneficiario.component.html',
-  styleUrls: ['./registrar-beneficiario.component.css']
+	selector: 'app-registrar-beneficiario',
+	templateUrl: './registrar-beneficiario.component.html',
+	styleUrls: ['./registrar-beneficiario.component.css']
 })
 export class RegistrarBeneficiarioComponent implements OnInit {
-
-	registrarBeneficiarioForm: FormGroup;
-
 	
-	
-
-
-	//Creo un beneficiario nuevo
+	registrarBeneficiario: FormGroup;
 	nuevoBeneficiario : Beneficiario;
+	convertidorDeDirecciones : AddressConverter;
+	
+	constructor(private beneficiarioApi: BeneficiarioApi, private route: ActivatedRoute , private router:Router) {
+		
+		this.nuevoBeneficiario =  new Beneficiario();
+		this.convertidorDeDirecciones = new AddressConverter();	
+		this.registrarBeneficiario = new FormGroup({
+			nombreOrganizacion: new FormControl(),
+			direccion: new FormControl(),
+			cantidadAtendidos: new FormControl(),
+			email: new FormControl('',[Validators.required,Validators.email]),
+			password: new FormControl(),
+			passwordConfirm: new FormControl()
+		});
+		
+	}
+	
+	onSubmit(){
+		console.warn(this.registrarBeneficiario.value);
+		this.nuevoBeneficiario =  new Beneficiario();
+		this.nuevoBeneficiario.username =  this.registrarBeneficiario.get("nombreOrganizacion").value;
+		this.nuevoBeneficiario.cantidadAtendidos=  this.registrarBeneficiario.get("cantidadAtendidos").value;
+		this.nuevoBeneficiario.email=  this.registrarBeneficiario.get("email").value;
+		this.nuevoBeneficiario.password=  this.registrarBeneficiario.get("password").value;
 
-  constructor(private beneficiarioApi: BeneficiarioApi, private route: ActivatedRoute , private router:Router) {
-	
-	this.nuevoBeneficiario =  new Beneficiario();
-	
-	this.registrarBeneficiarioForm = new FormGroup({
-       username: new FormControl(),
-	   adress: new FormControl(),
-	   cantAtendidos: new FormControl(),
-	   email: new FormControl(),
-	   password1: new FormControl(),
-	   password2: new FormControl()
-    });
+		// this.nuevoBeneficiario.ubicacion = this.generarUbicacion(this.registrarBeneficiario.get("direccion").value);
 
-  }
+		this.beneficiarioApi.create(this.nuevoBeneficiario).subscribe((beneficiarioCreado:Beneficiario)=>{
+			this.router.navigateByUrl("/login");
+			alert('Se registró exitosamente');		
+			// this.nuevoBeneficiario.ubicacion.beneficiarioId = beneficiarioCreado.id;
+			// this.ubicacionApi.create(this.nuevoBeneficiario.ubicacion).subscribe(()=>{	
+			//beneficiario.adress = this.registrarBeneficiarioForm.get("adress").value;
+			//Hay que trabajar la ubicacion
+			//this.itemApi.create(item).subscribe(()=>{
+			// this.router.navigateByUrl("/")
+			// })
+			//this.nuevoBeneficiario = new Beneficiario();
+			//console.log("Aca se tendria que crear un beneficiario nuevo");
+		})
+			
+	}
+		
 
-  onSubmit(){
-	  
-	 //var item=new Item(this.newItemForm.get("itemName").value)
-	var beneficiario = new Beneficiario();
-	beneficiario.username = this.registrarBeneficiarioForm.get("username").value;
-	beneficiario.cantidadAtendidos = this.registrarBeneficiarioForm.get("cantAtendidos").value;
-	beneficiario.email = this.registrarBeneficiarioForm.get("email").value;
 	
-	//Debería validarse el password, por ahora lo dejo asi
-	beneficiario.password = this.registrarBeneficiarioForm.get("password1").value;
-	
-	//beneficiario.adress = this.registrarBeneficiarioForm.get("adress").value;
-	//Hay que trabajar la ubicacion
-	
-	//this.itemApi.create(item).subscribe(()=>{
-     // this.router.navigateByUrl("/")
-     // })
-	this.beneficiarioApi.create(beneficiario).subscribe(()=>{this.router.navigateByUrl("/")})
-	
-	
-	//this.nuevoBeneficiario = new Beneficiario();
-	//console.log("Aca se tendria que crear un beneficiario nuevo");
-  }
-  
-  ngOnInit() {
-  }
 
+	generarUbicacion(direccion:string){
+		let nuevaUbicacion = new Ubicacion;
+		nuevaUbicacion.direccion = direccion;
+		nuevaUbicacion.puntoGeografico = this.convertidorDeDirecciones.coordinateForAddress(direccion);
+		return nuevaUbicacion;
+	}
+
+	get emailIsInvalid(){	
+		return this.registrarBeneficiario.get('email').dirty && !this.registrarBeneficiario.get('email').valid
+	}
+	
+	get passwordsNotEquals(){
+		return this.registrarBeneficiario.get('passwordConfirm').dirty && (this.registrarBeneficiario.get('password').value != this.registrarBeneficiario.get('passwordConfirm').value) 
+	}
+	
+	ngOnInit() {
+	}
+	
 }
