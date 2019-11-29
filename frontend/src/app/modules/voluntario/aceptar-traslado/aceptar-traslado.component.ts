2a6f37f13bd5c9ca59e2038cde5d08f608f04a09
Juan <jmarascutti@hotmail.com>
2019-11-29 18:17:48 -0300

    Incorporación de controlador de asignar-traslado

--- frontend/src/app/modules/voluntario/aceptar-traslado/aceptar-traslado.component.ts
+++ frontend/src/app/modules/voluntario/aceptar-traslado/aceptar-traslado.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Route } from '@angular/compiler/src/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { BALP } from '../../../_models/BALP';
+import { Traslado, Ubicacion  } from '../../../_services/lbservice/models';
+import { TrasladoApi, UbicacionApi } from '../../../_services/lbservice/services';
 
 @Component({
   selector: 'app-aceptar-traslado',
@@ -7,7 +14,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AceptarTrasladoComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  traslado : Traslado;
+  ubicacion: Ubicacion;
+
+  constructor(private apiTraslado:TrasladoApi, private apiUbicacion:UbicacionApi, private router: ActivatedRoute  ) { 
+    //this.idTraslado = route.snapshot.paramMap.get("idTraslado")
+
+    this.form = new FormGroup ({
+      // atributos del traslado
+      fecha : new FormControl(),
+      origen : new FormControl(),
+      destino : new FormControl(),
+      puntaje : new FormControl(),
+
+    })
+
+    
+
+  }
 
   ngOnInit() {
   }
--- frontend/src/app/modules/voluntario/asignar-traslado/asignar-traslado.component.ts
+++ frontend/src/app/modules/voluntario/asignar-traslado/asignar-traslado.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Route } from '@angular/compiler/src/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
 import { VoluntariosService } from 'src/app/_services/voluntarios.service';
-import { VehiculoApi, VoluntarioApi, TrasladoApi, DonanteApi, DonacionApi, DescripcionDetalladaApi, ProductoApi, TipoProductoApi } from '../../../_services/lbservice/services';
+import { BALP } from '../../../_models/BALP';
+import { VehiculoApi, VoluntarioApi, UbicacionApi, TrasladoApi, DonanteApi, DonacionApi, DescripcionDetalladaApi, ProductoApi, TipoProductoApi } from '../../../_services/lbservice/services';
 import { Vehiculo, Voluntario, Traslado, Ubicacion, Volumen, Donacion, EnvioParaBeneficiario } from '../../../_services/lbservice/models';
 @Component({
   selector: 'app-asignar-traslado',
@@ -11,17 +14,52 @@ import { Vehiculo, Voluntario, Traslado, Ubicacion, Volumen, Donacion, EnvioPara
 })
 export class AsignarTrasladoComponent implements OnInit {
 
-	idTraslado:string;
+	idTraslado: string;
+	form: FormGroup;
+  	traslado : Traslado;
+	ubicacion: Ubicacion;
+	voluntario: Voluntario;
 
 	/*
 		El usuario recibe un email como 
 		localhost:4200/asignar-traslado/laIdDeUnTraslado
 	*/
- 	constructor(private route:ActivatedRoute) {
- 		//Se obtiene de la URL
-  		this.idTraslado = route.snapshot.paramMap.get("idTraslado")
+ 	constructor(private apiVoluntario:VoluntarioApi, private apiTraslado:TrasladoApi, private apiUbicacion:UbicacionApi, private router: ActivatedRoute, private route:ActivatedRoute) {
+ 		//Obtengo el idTraslado por la URL
+		  this.idTraslado = route.snapshot.paramMap.get("idTraslado")
+		  
+		  this.form = new FormGroup ({
+			// atributos del traslado
+			fecha : new FormControl(),
+			origen : new FormControl(),
+			destino : new FormControl(),
+			puntaje : new FormControl(),
+		  });
 
+		// obtengo el voluntario que tiene iniciada la sesión
+		this.voluntario = apiVoluntario.getCachedCurrent();
+	
+		// obtengo el traslado a partir del idDeTraslado recibido por parámetro
+		apiTraslado.getTraslado(this.idTraslado, true).subscribe((trasladoRecuperado) => {
+			this.traslado = trasladoRecuperado;
+			// obtengo la dirección del traslado
+			apiTraslado.getUbicacion(this.idTraslado, true).subscribe((ubicacionDelTraslado) => {
+				this.ubicacion = ubicacionDelTraslado.dirección;
+			})
+		})
+		// SE MUESTRAN EN LA VISTA LOS DATOS DEL TRASLADO
  	}
+
+
+	 // DE ACA EN ADELANTE SE ASOCIA EL VOLUNTARIO AL TRASLADO
+	 onSubmit() {
+	 this.trasladoApi.update(this.idTraslado).subscribe((trasladoActualizado : Traslado) => {
+		// asocio al traslado en cuestión el id del voluntario que tiene la sesión iniciada
+		 this.traslado.voluntarioId = this.voluntario.id;
+		 alert('La asignación del traslado se registró correctamente');
+	}) 
+	 
+}
 
 	ngOnInit() {
 	}
