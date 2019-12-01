a708b7a98a9d4d3248d5aecdfea5563a6fd49483
Marascutti, Juan Marcos <jmarascutti@hotmail.com>
2019-11-30 22:54:08 -0300

    Avances el asignar-traslado. Se incorporaron mejoras y avances en el show del traslado

--- frontend/src/app/modules/voluntario/asignar-traslado/asignar-traslado.component.ts
+++ frontend/src/app/modules/voluntario/asignar-traslado/asignar-traslado.component.ts
@@ -5,8 +5,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
 import { VoluntariosService } from 'src/app/_services/voluntarios.service';
 import { BALP } from '../../../_models/BALP';
-import { VehiculoApi, VoluntarioApi, UbicacionApi, TrasladoApi, DonanteApi, DonacionApi, DescripcionDetalladaApi, ProductoApi, TipoProductoApi, EnvioParaBeneficiarioApi } from '../../../_services/lbservice/services';
-import { Vehiculo, Voluntario, Traslado, Ubicacion, Volumen, Donacion, EnvioParaBeneficiario } from '../../../_services/lbservice/models';
+import { VehiculoApi, VoluntarioApi, BeneficiarioApi, UbicacionApi, TrasladoApi, DonanteApi, DonacionApi, DescripcionDetalladaApi, ProductoApi, TipoProductoApi, EnvioParaBeneficiarioApi } from '../../../_services/lbservice/services';
+import { Vehiculo, Voluntario, Beneficiario, Donante, Traslado, Ubicacion, Volumen, Donacion, EnvioParaBeneficiario } from '../../../_services/lbservice/models';
 @Component({
   selector: 'app-asignar-traslado',
   templateUrl: './asignar-traslado.component.html',
@@ -17,8 +17,8 @@ export class AsignarTrasladoComponent implements OnInit {
 	idTraslado: string;
 	form: FormGroup;
   	traslado : Traslado;
-	origen: Ubicacion;
-	destino: Ubicacion;
+	origen: String;
+	destino: String;
 	voluntario: Voluntario;
 	balp : BALP = new BALP;
 
@@ -26,7 +26,7 @@ export class AsignarTrasladoComponent implements OnInit {
 		El usuario recibe un email como 
 		localhost:4200/asignar-traslado/laIdDeUnTraslado
 	*/
- 	constructor(private apiEnvio:EnvioParaBeneficiarioApi, private apiDonacion:DonacionApi,private apiVoluntario:VoluntarioApi, private apiTraslado:TrasladoApi, private apiUbicacion:UbicacionApi, private router: ActivatedRoute, private route:ActivatedRoute) {
+ 	constructor(private apiEnvio:EnvioParaBeneficiarioApi, private apiDonante:DonanteApi ,private apiDonacion:DonacionApi,private apiVoluntario:VoluntarioApi, private apiTraslado:TrasladoApi, private apiUbicacion:UbicacionApi, private router: ActivatedRoute, private route:ActivatedRoute) {
  		//Obtengo el idTraslado por la URL
 		  this.idTraslado = route.snapshot.paramMap.get("idTraslado")
 		  this.form = new FormGroup ({
@@ -49,52 +49,55 @@ export class AsignarTrasladoComponent implements OnInit {
 					2. El traslado es de un envio para beneficiario, entonces el origen es el banco y el destino es un beneficiario
 			*/
 			if (this.traslado.tipo == 'donacion'){
-				//Caso 1 (donacion)
+				//Caso 1 (donación)
 				//1. Recuperar la donacion trasladada al banco
 				apiDonacion.findById(this.traslado.idDonacionTrasladadaAlBanco).subscribe((donacion:Donacion) => {
 					//2. Recuperar el donante de esa donacion 
-					//apiDonacion.findById(donacion.idDonante)...
-						//3. Recuperar la ubicación de ese donante
-						//apiUbicacion.findById(...).subscribe((ubicacionRecuperada:Ubicacion)=>{
+					apiDonacion.findById(donacion.idDonante).subscribe((donanteRecuperado:Donante) => { // obtuve al donante
+						//3. Recuperar la ubicación de ese donante (busco la ubicación del donante con el id del donanteRecuperado)
+				     	apiUbicacion.findById(donanteRecuperado.id).subscribe((ubicacionRecuperada:Ubicacion)=>{  // obtuve la ubicación del donante				
 							//4. Guardar la direccion como origen
-							//this.origen = ubicacionRecuperada;
+							this.origen = ubicacionRecuperada.direccion;
 							//5. Guardar la dirección del banco como destino
-							this.destino = this.balp.ubicacionBALP;
-						//}) //Fin ubicacion
-
+							this.destino = this.balp.ubicacionBALP.direccion;						
+					   })
+					}) 
 				}) //Fin donacion
 			} else {
 				//Caso 2 (envio)
 				//1. Recuperar el envio trasladado a un beneficiario
+				apiEnvio.findById(this.traslado.idEnvioTrasladadoAUnBeneficiario).subscribe((envio:EnvioParaBeneficiario) => {
 					//2. Recuperar el beneficiario de ese envio
+					apiEnvio.findById(envio.beneficiarioId).subscribe((beneficiarioRecuperado:Beneficiario) => {
 						//3. Recuperar la ubicacion de ese beneficiario
+						apiUbicacion.findById(beneficiarioRecuperado.id).subscribe((ubicacionRecuperada:Ubicacion)=>{
 							//4. Guardar la direccion como destino
+							this.destino = ubicacionRecuperada.direccion;
 							//5. Guardar la direccion del banco como origen
+							this.origen = this.balp.ubicacionBALP.direccion;
+						})
+					})				
+				}) // Fin envío																	
 			} //Fin del if/else
+ 		}) // Fin de la primer promesa de todas
+	 } 	// Fin del constructor
+	
+	// SE MUESTRAN EN LA VISTA LOS DATOS DEL TRASLADO (¡¡POR FIN!!)
 
+			// DE ACA EN ADELANTE SE ASOCIA EL VOLUNTARIO AL TRASLADO
+			onSubmit() {
 
+				/* El update es un poco mas complicado y no lo se hacer bien todavia, lo dejo comentado asi compila
 
-
-		})
-		// SE MUESTRAN EN LA VISTA LOS DATOS DEL TRASLADO
- 	}
-
-
-	 // DE ACA EN ADELANTE SE ASOCIA EL VOLUNTARIO AL TRASLADO
-	 onSubmit() {
-
-	 	/* El update es un poco mas complicado y no lo se hacer bien todavia, lo dejo comentado asi compila
-
-		 this.trasladoApi.update(this.idTraslado).subscribe((trasladoActualizado : Traslado) => {
-			// asocio al traslado en cuestión el id del voluntario que tiene la sesión iniciada
-			 this.traslado.voluntarioId = this.voluntario.id;
-			 alert('La asignación del traslado se registró correctamente');
-		}) 
-		*/
-	 
-}
+				this.trasladoApi.update(this.idTraslado).subscribe((trasladoActualizado : Traslado) => {
+				// asocio al traslado en cuestión el id del voluntario que tiene la sesión iniciada
+					this.traslado.voluntarioId = this.voluntario.id;
+					alert('La asignación del traslado se registró correctamente');
+			}) 
+			*/ 
+			}
 
 	ngOnInit() {
-	}
+	}	
 
 }
