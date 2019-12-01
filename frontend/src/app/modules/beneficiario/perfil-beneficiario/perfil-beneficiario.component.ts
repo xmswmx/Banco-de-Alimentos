8e9a4c174510eda42c9ebfcf46f5ab81761d0217
Emanuel <emanuelbastons@gmail.com>
2019-12-01 12:01:37 -0300

    Fix arranco el perfil-beneficiario
    El que estaba antes era editar-beneficiario, que sirvio para mostrar el CRUD nada mas

--- frontend/src/app/modules/beneficiario/perfil-beneficiario/perfil-beneficiario.component.ts
+++ frontend/src/app/modules/beneficiario/perfil-beneficiario/perfil-beneficiario.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { LoopBackConfig, BaseLoopBackApi } from '../../../_services/lbservice';
-import { Beneficiario } from '../../../_services/lbservice/models';  
+import { Beneficiario, Ubicacion } from '../../../_services/lbservice/models';  
 import { Route } from '@angular/compiler/src/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
-import { BeneficiarioApi } from '../../../_services/lbservice/services';
+import { BeneficiarioApi, UbicacionApi } from '../../../_services/lbservice/services';
 
 @Component({
   selector: 'app-perfil-beneficiario',
@@ -15,37 +15,23 @@ export class PerfilBeneficiarioComponent implements OnInit {
 
 	editarBeneficiarioForm: FormGroup;
 	loggedBeneficiario: Beneficiario;
-  constructor(private beneficiarioApi: BeneficiarioApi, private route: ActivatedRoute , private router:Router) {
+	username: string;
+	address: string = 'Cargando..';
+	cantAtendidos: number;
+	email: string;
+  constructor(private ubiApi: UbicacionApi,private beneficiarioApi: BeneficiarioApi, private route: ActivatedRoute , private router:Router) {
 		
-	this.editarBeneficiarioForm = new FormGroup({
-       username: new FormControl(),
-	   adress: new FormControl(),
-	   cantAtendidos: new FormControl(),
-	   email: new FormControl(),
-	   password: new FormControl(),
-    });
 	this.loggedBeneficiario = beneficiarioApi.getCachedCurrent();
+	this.beneficiarioApi.getUbicacion(this.loggedBeneficiario.id,true).subscribe((ubicacion:Ubicacion)=>{
+		this.address = ubicacion.direccion;
+	})
+	this.username = this.loggedBeneficiario.username;
+	this.cantAtendidos = this.loggedBeneficiario.cantidadAtendidos;
+	this.email = this.loggedBeneficiario.email;
+
 
 	}
-
-onSubmit(){
-	  
-	this.loggedBeneficiario.username = this.editarBeneficiarioForm.get("username").value;
-	this.loggedBeneficiario.cantidadAtendidos = this.editarBeneficiarioForm.get("cantAtendidos").value;
-	//this.loggedBeneficiario.direccion = this.editarBeneficiarioForm.get("adress").value;
-	this.loggedBeneficiario.email = this.editarBeneficiarioForm.get("email").value;
-	this.loggedBeneficiario.password = this.editarBeneficiarioForm.get("password").value;
-	
-	this.beneficiarioApi.upsert(this.loggedBeneficiario).subscribe(()=>{this.router.navigateByUrl("/")})
-	
-  }
-  
- borrar(){
-	 //Hay un problemita en esta linea, se hacen 2 cosas: se borra la cuenta y se logout.. pero como puedo hacer logout si mi cuenta no existe?
-	 //Tengo que buscar alguna forma de limpiar la cache del navegador antes de cargar otra pagina
-	 //Igual anda con posibilidad de un eventual bug :)
-	 this.beneficiarioApi.deleteById(this.loggedBeneficiario.id).subscribe(()=>{this.beneficiarioApi.logout();this.router.navigateByUrl("/")});
- }
+ 
  
   ngOnInit() {
   }
