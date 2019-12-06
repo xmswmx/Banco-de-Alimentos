7640d45a90080af2d3a60d7ba934c70beada20e7
Emanuel <emanuelbastons@gmail.com>
2019-12-06 14:03:25 -0300

    Agrego clasificacion

--- frontend/src/app/modules/topDonaciones/ver-top/ver-top.component.ts
+++ frontend/src/app/modules/topDonaciones/ver-top/ver-top.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Location} from '@angular/common';
+import { DonanteApi, VoluntarioApi } from '../../../_services/lbservice/';
+import { Donante, Voluntario } from '../../../_services/lbservice'
 
 @Component({
   selector: 'app-ver-top',
@@ -8,8 +10,29 @@ import {Location} from '@angular/common';
 })
 export class VerTopComponent implements OnInit {
 
-  constructor(private _location:Location) { }
+	voluntarios = [];
+	donantes = [];
+  constructor(private _location:Location,
+  			  private apiDonante:DonanteApi,
+  			  private apiVoluntario:VoluntarioApi) 
+  { 
+  	apiDonante.find().subscribe((donantes)=>{
+  		this.donantes = donantes;
+  		apiVoluntario.find().subscribe((voluntarios)=>{
+  			this.voluntarios=voluntarios;
+  			this.voluntarios = this.voluntarios.sort( this.compararVoluntarios).slice(0,10);
+  			this.donantes= this.donantes.sort(this.compararDonantes).slice(0,10);
+  		})
+  	})
 
+  }
+
+  compararVoluntarios(vol1:Voluntario, vol2:Voluntario){
+  	return vol2.puntuacion - vol1.puntuacion;
+  }
+  compararDonantes(d1,d2){
+  	return d2.puntuacion-d1.puntuacion;
+  }
   ngOnInit() {
   }
 
