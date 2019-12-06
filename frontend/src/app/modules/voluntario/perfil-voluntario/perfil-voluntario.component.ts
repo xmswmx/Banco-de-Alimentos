5f1bd7feb104292c78cd9a4ad15f613fd1f9cd01
Emanuel <emanuelbastons@gmail.com>
2019-12-06 11:05:57 -0300

    Agrego ver puntos e insignias en perfil de voluntario

--- frontend/src/app/modules/voluntario/perfil-voluntario/perfil-voluntario.component.ts
+++ frontend/src/app/modules/voluntario/perfil-voluntario/perfil-voluntario.component.ts
@@ -3,8 +3,10 @@ import { Injectable } from '@angular/core';
 import { Route } from '@angular/compiler/src/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
-import { Voluntario, Vehiculo, Volumen, Ubicacion } from '../../../_services/lbservice/models';
-import { VoluntarioApi, VehiculoApi, VolumenApi, UbicacionApi } from '../../../_services/lbservice/services';
+import { Voluntario, Vehiculo, Volumen, Ubicacion, Insignia, TipoInsignia } from '../../../_services/lbservice/models';
+import { VoluntarioApi, VehiculoApi, VolumenApi, UbicacionApi, InsigniaApi, TipoInsigniaApi } from '../../../_services/lbservice/services';
+import { faStar, faCandyCane, faGift, faEgg, faGifts , faHandsHelping, faDonate } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faFacebook,faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 
 @Component({
@@ -20,9 +22,31 @@ export class PerfilVoluntarioComponent implements OnInit {
   ubicacion: string;
   form: FormGroup;
 
-  constructor(private apiVoluntario: VoluntarioApi, apiVehiculo: VehiculoApi, apiVolumen: VolumenApi, private router: Router) {
+      //Estos son los iconos
+    faCandyCane=faCandyCane;
+    faGift=faGift;
+    faGifts=faGifts;
+    faHandsHelping=faHandsHelping;
+    faEgg=faEgg;
+    faDonate=faDonate;
+    faStar=faStar;
+
+    iconos = [];
+    badges = []; //Se 0:Nombre, 1:icono, 2:fechaOtorgada, 3:vto
+
+  constructor(private apiInsignia: InsigniaApi, private apiVoluntario: VoluntarioApi, apiVehiculo: VehiculoApi, apiVolumen: VolumenApi, private router: Router) {
    
-   
+    //Esto es como un diccionario que uso para convertir texto de la api en iconos mostrables
+    this.iconos=[
+    ['faEgg',faEgg],
+    ['faCandyCane',faCandyCane],
+    ['faGift',faGift],
+    ['faGifts',faGifts],
+    ['faHandsHelping',faHandsHelping],
+    ['faDonate',faDonate],
+    ['faStar',faStar],
+    ];
+
     this.form = new FormGroup({
       // atributos del voluntario
       nombre: new FormControl(),
@@ -56,7 +80,21 @@ export class PerfilVoluntarioComponent implements OnInit {
       apiVehiculo.getVolumen(this.vehiculo.id, true).subscribe((volumen) => {
         this.volumen = volumen;
         apiVoluntario.getUbicacion(this.voluntario.id,true).subscribe((ubicacion)=>{	
-          this.ubicacion = ubicacion.direccion;					
+          this.ubicacion = ubicacion.direccion;	
+          apiVoluntario.getInsignia(apiVoluntario.getCachedCurrent().id).subscribe((insignias:Insignia[])=>{
+            for (let insignia of insignias){
+              apiInsignia.getTipoInsignia(insignia.id,true).subscribe((tipodeinsignia:TipoInsignia)=>{
+                  let parNombreIcono = this.iconos.find(elemento => elemento[0] == tipodeinsignia.imagen)
+                  let icono = parNombreIcono[1];
+                  this.badges.push([
+                     tipodeinsignia.nombre,
+                     icono,
+                     insignia.fechaOtorgada,
+                     insignia.fechaVencimiento
+                    ]);
+                })            
+              } //Termina el for
+          })				
       })
 
     })
