a242073eca75d50b89c78e8d3047656a1ecf6aee
Emanuel <emanuelbastons@gmail.com>
2019-12-08 21:15:27 -0300

    Agrego componente de lista de items al envio

--- frontend/src/app/modules/admin/nuevo-envio/nuevo-envio-principal/nuevo-envio-principal.component.ts
+++ frontend/src/app/modules/admin/nuevo-envio/nuevo-envio-principal/nuevo-envio-principal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Item } from '../../../../_services/lbservice/models';
 
 @Component({
   selector: 'app-nuevo-envio-principal',
@@ -13,6 +14,7 @@ export class NuevoEnvioPrincipalComponent implements OnInit {
 	var: string = 'hola';
   idBeneficiario:string = 'Sin seleccionar';
   idDonacion:string = 'Sin seleccionar';
+  itemList:Item[]=[];
   constructor() {	}
 
 
@@ -23,6 +25,11 @@ export class NuevoEnvioPrincipalComponent implements OnInit {
   }
   onEnviarIdDonacion(id:string){
     this.idDonacion = id;
+  }
+  onEnviarItems(items:Item[]){
+    this.itemList = items;
+    console.log("Se actualizo el item list del componente padre");
+    console.log(this.itemList);
   }
 
 }
