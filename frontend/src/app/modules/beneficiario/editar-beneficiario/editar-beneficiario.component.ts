de1aec9fea274cfc32f2687098e340ce09c9c6fa
matiasmosquera <matias.mosquera@hotmail.com.ar>
2019-12-13 17:28:18 -0300

    Se agregaron validaciones en editar beneficiario

--- frontend/src/app/modules/beneficiario/editar-beneficiario/editar-beneficiario.component.ts
+++ frontend/src/app/modules/beneficiario/editar-beneficiario/editar-beneficiario.component.ts
@@ -2,45 +2,63 @@ import { Component, OnInit } from '@angular/core';
 import { Beneficiario, Ubicacion } from '../../../_services/lbservice/models';
 import { Router, ActivatedRoute } from '@angular/router';
 import { BeneficiarioApi, UbicacionApi } from '../../../_services/lbservice/services';
+import { FormGroup, FormControl, NgForm, Validators } from '@angular/forms';
+
 
 //Revisar si se usa
 import { LoopBackConfig, BaseLoopBackApi } from '../../../_services/lbservice';
 import { Route } from '@angular/compiler/src/core';
-import { FormGroup, FormControl, NgForm } from '@angular/forms';
+
 
 @Component({
-  selector: 'editar-beneficiario',
-  templateUrl: './editar-beneficiario.component.html',
-  styleUrls: ['./editar-beneficiario.component.css']
+	selector: 'editar-beneficiario',
+	templateUrl: './editar-beneficiario.component.html',
+	styleUrls: ['./editar-beneficiario.component.css']
 })
+
 export class EditarBeneficiarioComponent implements OnInit {
-	// editarBeneficiarioForm: FormGroup;
-  beneficiario: Beneficiario;
-	username: string;
-	address: string = 'Cargando..';
-	cantAtendidos: number;
-	email: string;
+	beneficiario: Beneficiario;
+	direccion: string = 'Cargando..';  
+	formBeneficiario: FormGroup;
 
 
-  constructor(private ubiApi: UbicacionApi, private beneficiarioApi: BeneficiarioApi, private route: ActivatedRoute, private router: Router) {
-    
-    this.beneficiario = beneficiarioApi.getCachedCurrent();
+	constructor(private ubiApi: UbicacionApi, private beneficiarioApi: BeneficiarioApi, private route: ActivatedRoute, private router: Router) {
+		
+		this.beneficiario = beneficiarioApi.getCachedCurrent();
 		this.beneficiarioApi.getUbicacion(this.beneficiario.id, true).subscribe((ubicacion: Ubicacion) => {
-			this.address = ubicacion.direccion;	})
-		this.username = this.beneficiario.username;
-		this.cantAtendidos = this.beneficiario.cantidadAtendidos;
-		this.email = this.beneficiario.email;
+			this.direccion = ubicacion.direccion;	})
+			this.formBeneficiario = new FormGroup({
+				username : new FormControl('',[Validators.required,
+					Validators.minLength(4)]),
+				direccion : new FormControl(),
+				cantidadAtendidos : new FormControl(),
+				email: new FormControl('', [Validators.required, Validators.email]),
+			});
+	}
+	get username() {
+		return this.formBeneficiario.get('username')
+	}
+	get direccionForm() {
+		return this.formBeneficiario.get('direccion')
+	}
+	get email() {
+		return this.formBeneficiario.get('email')
+	}	
+	get cantidadAtendidos() {
+		return this.formBeneficiario.get('cantidadAtendidos')
+	}
+	get emailIsInvalid() {
+		return this.formBeneficiario.get('email').dirty && !this.formBeneficiario.get('email').valid
+	}
 
-  }
+	ngOnInit() {	
+	}
 
-  ngOnInit() {
-  }
+	onSubmit(beneficiarioForm: NgForm){
+		console.log(beneficiarioForm.value);
+		console.log(beneficiarioForm.valid);
+	}
 
-  onSubmit(beneficiarioForm: NgForm){
-    console.log(beneficiarioForm.value);
-    console.log(beneficiarioForm.valid);
-
-  }
 
 
 }
--- frontend/src/app/modules/beneficiario/registrar-beneficiario/registrar-beneficiario.component.ts
+++ frontend/src/app/modules/beneficiario/registrar-beneficiario/registrar-beneficiario.component.ts
@@ -16,9 +16,8 @@ import { Route } from '@angular/compiler/src/core';
 	styleUrls: ['./registrar-beneficiario.component.css']
 })
 export class RegistrarBeneficiarioComponent implements OnInit {
-
-	registrarBeneficiario: FormGroup;
 	nuevoBeneficiario: Beneficiario;
+	registrarBeneficiario: FormGroup;
 	convertidorDeDirecciones: AddressConverter;
 
 	constructor(private ubicacionApi:UbicacionApi, private beneficiarioApi: BeneficiarioApi, private route: ActivatedRoute, private router: Router) {
