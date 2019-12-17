581d70a0c9ad53d32f9d5412c10b3415b0839fed
Emanuel <emanuelbastons@gmail.com>
2019-12-17 15:50:55 -0300

    Insignia estrella de oro codeada

--- frontend/src/app/_services/insignias.service.ts
+++ frontend/src/app/_services/insignias.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Traslado, TipoInsignia, TipoInsigniaApi, Insignia, InsigniaApi, VoluntarioApi, DonanteApi } from './lbservice';
+import { Voluntario, Donante, Traslado, TipoInsignia, TipoInsigniaApi, Insignia, InsigniaApi, VoluntarioApi, DonanteApi } from './lbservice';
 
 @Injectable({
   providedIn: 'root'
@@ -68,7 +68,7 @@ export class InsigniasService {
 	onTraslado(idVoluntario:string):Promise<boolean>{
 		console.clear();
 		let insigniasDelVoluntario : Insignia[];
-		let insigniasPendientes = 4; //Aumentar este contador cada vez que se crea una medalla nueva
+		let insigniasPendientes = 5; //Aumentar este contador cada vez que se crea una medalla nueva
 		return new Promise((resolve,reject)=>{
 			console.log("Se entra en la promesa");
 			this.voluntarioApi.getInsignia(idVoluntario).subscribe((insignias:Insignia[])=>{
@@ -215,20 +215,73 @@ export class InsigniasService {
 				})
 
 
-				console.log("Checkeo de estrella dorada (Aun no esta funcionando");
 				//Check estrella de oro
-					//Contar dias en top
-					//son 30+?
-						//no tiene la insignia?
-							//Crear la insignia para quien corresponda
-					//Actualizar top
-					//insignias pendientes == 1?
-						//resolver callback
-					//else faltan insignas
-						//decrementar una
-
-
-
+				console.log("Checkeo de estrella dorada (Aun no esta funcionando");
+				if (this.diasEntre(this.ingresoVoluntario,fechaActual)>=30){
+					if (insigniasDelVoluntario.some(unaInsignia => unaInsignia.tipoInsigniaId 
+							== this.getTipoInsigniaNombre("estrella de oro").id)){
+						this.voluntarioApi.find({order: 'puntaje DESC'}).subscribe((voluntarios:Voluntario[])=>{
+							console.log("Se obtuvo el top ordenado de mayor a menor");
+							voluntarios[0];
+							if (voluntarios[0].id != this.voluntario1){
+								console.log("Como el #1 no coincide con el #1 que estaba registrado se actualiza el #1");
+								this.voluntario1 = voluntarios[0].id;
+								this.ingresoVoluntario = new Date();
+							}
+							if (insigniasPendientes == 1){
+								console.log("Era la ultima insignia.")
+								resolve(true);
+							} else {
+								insigniasPendientes--;
+								console.log("Quedan mas insignias, continua iterando");
+							}
+						})
+					} else {
+						console.log("El que va primero no tiene la insignia");
+						let insignia = new Insignia();
+						insignia.tipoInsigniaId = this.getTipoInsigniaNombre("estrella de oro").id;
+						insignia.voluntarioId = this.voluntario1;
+						insignia.fechaOtorgada = new Date();
+						insignia.fechaVencimiento = new Date(2500, 11, 24, 10, 33, 30, 0);
+						this.insigniaApi.create(insignia).subscribe(()=>{
+							console.log("Se creo la insignia estrella de oro para el voluntario que iba primero");
+						this.voluntarioApi.find({order: 'puntaje DESC'}).subscribe((voluntarios:Voluntario[])=>{
+							console.log("Se obtuvo el top ordenado de mayor a menor");
+							voluntarios[0];
+							if (voluntarios[0].id != this.voluntario1){
+								console.log("Como el #1 no coincide con el #1 que estaba registrado se actualiza el #1");
+								this.voluntario1 = voluntarios[0].id;
+								this.ingresoVoluntario = new Date();
+							}
+							if (insigniasPendientes == 1){
+								console.log("Era la ultima insignia.")
+								resolve(true);
+							} else {
+								insigniasPendientes--;
+								console.log("Quedan mas insignias, continua iterando");
+							}
+						})
+						})
+					}
+				} else {
+					console.log("No pasaron 30 dias desde que ingreso a top 1 el voluntario que está primero");
+					this.voluntarioApi.find({order: 'puntaje DESC'}).subscribe((voluntarios:Voluntario[])=>{
+						console.log("Se obtuvo el top ordenado de mayor a menor");
+						voluntarios[0];
+						if (voluntarios[0].id != this.voluntario1){
+							console.log("Como el #1 no coincide con el #1 que estaba registrado se actualiza el #1");
+							this.voluntario1 = voluntarios[0].id;
+							this.ingresoVoluntario = new Date();
+						}
+						if (insigniasPendientes == 1){
+							console.log("Era la ultima insignia.")
+							resolve(true);
+						} else {
+							insigniasPendientes--;
+							console.log("Quedan mas insignias, continua iterando");
+						}
+					})
+				}
 			})
 		})
 	}
@@ -277,6 +330,11 @@ export class InsigniasService {
 	getTipoInsigniaNombre(nombre:string):TipoInsignia{
 		//La idea es devolver la id de la insignia con dado nombre
 		return this.tiposDeInsignia.find(tipo => tipo.nombre == nombre);
+	}
+	diasEntre(diaViejo:Date,diaReciente:Date):number{
+		let tiempo = diaReciente.getTime()-diaViejo.getTime();
+		let dias = Math.round(tiempo / (1000*3600*24));
+		return dias;
 	}
 }
 
